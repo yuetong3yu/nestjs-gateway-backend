4ad386419044541ae8d9f7cd3ced467f803ee70f
chris.yu <yuetong3yu@gmail.com>
2024-02-27 00:02:26 +1100

    add handleMessage function

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,4 +1,17 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 
 @WebSocketGateway()
-export class ChatGateway {}
+export class ChatGateway {
+  @WebSocketServer()
+  private readonly server;
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    this.server.emit('message', message);
+  }
+}
